ae143d231b065eb92f96edaba4c5d730378c6ca7
あいるぅ <contact.airu@gmail.com>
2025-03-26 04:10:39 +0900

    全キャラ表示の修正

--- characters.js
+++ characters.js
@@ -10,12 +10,17 @@ async function loadCharacters() {
 
 function displayCharacters(characters) {
     const container = document.getElementById("character-list");
-    container.innerHTML = characters.map(char =>
-        `<div class="character-card">
-            <img src="${char.image.replace(/・/g, "%E3%83%BB")}" alt="${char.name}">
-            <p>${char.name}</p>
-        </div>`
-    ).join("");
+    container.innerHTML = characters.map(char => {
+        // ファイル名からキャラ名を推測（拡張子とパスを除去）
+        let extractedName = char.image.replace(/^images\//, "").replace(/\.\w+$/, "");
+
+        return `<div class="character-card">
+            <img src="${char.image.replace(/・/g, "%E3%83%BB")}" alt="${extractedName}">
+            <p>名前: ${extractedName}</p>
+            <p>攻撃力: ${char.attack}</p>
+        </div>`;
+    }).join("");
 }
+
 
 loadCharacters();
